fc0be42c9bef709f0267ce61deb3d284f36e99b9
Taha <tahaerdemozturk@gmail.com>
2024-05-29 23:18:07 -0400

    Footer

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ window.addEventListener('load', function() {
 let backArrow = document.getElementsById("back-arrow");
 window.onscroll = function() {moveTop()};
 
-function moveTop() {
+function scrollFunction() {
   if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
     backArrow.style.display = "block";
   } else {
@@ -67,14 +67,3 @@ function moveTop() {
 function moveTop() {
     window.scrollTo({ top: 0, behavior: 'smooth', easing: 'ease-in' });
 }
-
-function moveTop() {
-    if (document.body.scrollTop > 2000 || document.documentElement.scrollTop > 2000) {
-      backArrow.style.display = "block";
-    } else {
-      backArrow.style.display = "none";
-    }
-  }
-  function moveTop() {
-      window.scrollTo({ top: 0, behavior: 'smooth', easing: 'ease-in' });
-  }
